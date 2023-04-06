106d58263336fca83934da725cccec410e0e9b08
ESTHEREKANEM <estekanem1@gmail.com>
2023-04-06 05:00:20 -0700

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,65 @@
-#!usr/bin/env bash
-#The following script set ups webservers for deployment of web static
+#!/usr/bin/env bash
+# Installs, configures, and starts the web server
+SERVER_CONFIG="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
 
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
+
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
+
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
+
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
+
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}"
+HOME_PAGE="<!DOCTYPE html>
+<html lang='en-US'>
+	<head>
+		<title>Home - AirBnB Clone</title>
+	</head>
+	<body>
+		<h1>Welcome to AirBnB!</h1>
+	<body>
+</html>
+"
+# shellcheck disable=SC2230
 while ! [ -x "$(command -v nginx)" ]; do
   sudo apt-get update
   sudo apt-get install nginx -y
 done
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+mkdir -p /var/www/html /var/www/error
+chmod -R 755 /var/www
+echo 'Hello World!' > /var/www/html/index.html
+echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
 
-# Creating a fake HTML file
-echo "<html><head><title>Test Page</title></head><body><p>This is a test page.</p></body></html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-
-config="server {
-    listen 80;
-    listen [::]:80;
-    location /hbnb_static {
-        alias /data/web_static/current/;
-        index index.html;
-    }
-}"
-sudo echo "$config" | sudo tee /etc/nginx/sites-available/default > /dev/null
-sudo service nginx restart
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "$HOME_PAGE" > /data/web_static/releases/test/index.html
+[ -d /data/web_static/current ] && rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+while true; do
+    if [ "$(pgrep -c nginx)" -le 0 ]; then
+        service nginx start
+    else
+        service nginx restart
+    fi
+    sleep 5
+done
